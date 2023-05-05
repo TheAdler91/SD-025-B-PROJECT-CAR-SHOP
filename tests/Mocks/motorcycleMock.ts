d234bb7bf629c28200b733038c9ee3c3f2be9f35
Your NamePhilip <philip.adler@hotmail.com>
2023-05-05 17:00:36 -0300

    Test: Added tests for motorcycle

---
+++ tests/Mocks/motorcycleMock.ts
@@ -0,0 +1,43 @@
+export const motorcycleInput = {
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+export const motorcycleOutput = {
+  id: '634852326b35b59438fbea2f',
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+export const motorcycleArray = [
+  {
+    id: 'd71c74dd99b55f9b9ecbb8d3',
+    model: 'Yamaha MT-07',
+    year: 2020,
+    color: 'Black',
+    status: false,
+    buyValue: 45000,
+    category: 'Naked',
+    engineCapacity: 700,
+  },
+  {
+    id: '74e6bb4e6dcd9837cddaed42',
+    model: 'Suzuki GSX-S750',
+    year: 2018,
+    color: 'Black',
+    status: true,
+    buyValue: 50.000,
+    category: 'Sport',
+    engineCapacity: 750,
+  },
+]; 
--- tests/unit/Services/motorcycle.test.ts
+++ tests/unit/Services/motorcycle.test.ts
@@ -1,40 +1,71 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { Model } from 'mongoose';
+import MotorcycleODM from '../../../src/Models/MotorcycleODM';
 import MotorcycleService from '../../../src/Services/MotorcycleService';
+import Motorcycle from '../../../src/Domains/Motorcycle';
+import { motorcycleInput, motorcycleOutput, motorcycleArray } from '../../Mocks/motorcycleMock';
 
-describe(' Testing MotorcycleService', function () {
-  it('Should create a motorcycle correctly', async function () {
-    const motorcycleInput = {
-      model: 'Honda Cb 600f Hornet',
-      year: 2005,
-      color: 'Yellow',
-      status: true,
-      buyValue: 30.000,
-      category: 'Street',
-      engineCapacity: 600,
-    };
-
-    const motorcycleOutput = {
-      id: '634852326b35b59438fbea2f',
-      model: 'Honda Cb 600f Hornet',
-      year: 2005,
-      color: 'Yellow',
-      status: true,
-      buyValue: 30.000,
-      category: 'Street',
-      engineCapacity: 600,
-    };
-
-    sinon.stub(Model, 'create').resolves(motorcycleOutput);
-
-    const service = new MotorcycleService();
-    const result = await service.createMotorcycle(motorcycleInput);
-
-    expect(result).to.be.deep.equal(motorcycleOutput);
-  });
-
+describe('Testing MotorcycleService', function () {
   afterEach(function () {
     sinon.restore();
+  });
+  describe('createMotorcycle', function () {
+    it('Should create a motorcycle correctly', async function () {
+      sinon.stub(MotorcycleODM.prototype, 'create').resolves(motorcycleOutput);
+      const service = new MotorcycleService();
+      const result = await service.createMotorcycle(motorcycleInput);
+      expect(result).to.be.deep.equal(motorcycleOutput);
+    });
+  });
+
+  describe('getAllMotorcycles', function () {
+    it('Should return an array of motorcycles', async function () {
+      sinon.stub(MotorcycleODM.prototype, 'findAll').resolves(motorcycleArray);
+      const service = new MotorcycleService();
+      const result = await service.getAllMotorcycles();
+      expect(result).to.be.deep.equal(motorcycleArray
+        .map((motorcycle) => new Motorcycle(motorcycle)));
+    });
+  });
+
+  describe('getMotorcycleById', function () {
+    it('Should return the motorcycle with the specified ID', async function () {
+      sinon.stub(MotorcycleODM.prototype, 'findById').resolves(motorcycleOutput);
+      const service = new MotorcycleService();
+      const result = await service.getMotorcycleById(motorcycleOutput.id);
+      expect(result).to.be.deep.equal(new Motorcycle(motorcycleOutput));
+    });
+
+    it('Should throw an error if the motorcycle is not found', async function () {
+      sinon.stub(MotorcycleODM.prototype, 'findById').resolves(null);
+      const service = new MotorcycleService();
+      try {
+        await service.getMotorcycleById('64491d779158ff9021c96');
+      } catch (error) {
+        expect(error).to.be.an('error');
+      }
+    });
+
+    describe('updateMotorcycle', function () {
+      it('Should update the motorcycle with the specified ID', async function () {
+        sinon.stub(MotorcycleODM.prototype, 'findById').resolves(motorcycleOutput);
+        sinon.stub(MotorcycleODM.prototype, 'update').resolves(motorcycleOutput);
+        const service = new MotorcycleService();
+        const result = await service.updateMotorcycle(motorcycleOutput.id, motorcycleInput);
+        expect(result).to.be.deep.equal(new Motorcycle(motorcycleOutput));
+      });
+    
+      it('Should throw an error if update a motorcycle with an invalid id', async function () {
+        sinon.stub(MotorcycleODM.prototype, 'findById').resolves(null);
+        const service = new MotorcycleService();
+        const invalidId = '9898787784656';
+          
+        try {
+          await service.updateMotorcycle(invalidId, motorcycleInput);
+        } catch (error) {
+          expect(error).to.be.an('error');
+        }
+      });
+    });
   });
 });
